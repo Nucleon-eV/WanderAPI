6549e2d85ec098680a0e84ee66b2dfc43f52b06d
MTRNord <mtrnord1@gmail.com>
2019-03-24 17:43:42 +0100

    Disable TLS

--- src/main.rs
+++ src/main.rs
@@ -109,7 +109,7 @@ fn main() {
 
     info!("Listening on 127.0.0.1:[YOUR_PORT]");
 
-    let state = warp::any().map(move || Context { db: Connection::connect("postgres://postgres@localhost:5433", TlsMode::Prefer(&NativeTls::new().unwrap())).unwrap() });
+    let state = warp::any().map(move || Context { db: Connection::connect("postgres://postgres@localhost:5433", TlsMode::None).unwrap() });
     let graphql_filter = juniper_warp::make_graphql_filter(schema(), state.boxed());
 
     warp::serve(
